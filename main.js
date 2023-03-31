60cdd3809045e7c3a7e7be25e6065d743615dbf0
sharmanandika <101844758+sharmanandika@users.noreply.github.com>
2023-03-31 19:45:05 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function preload(){
+
+}
+
+function setup(){
+canvas=canvasCreate(300,300);
+canvas.center();
+}
+
+function draw(){
+
+}
+
+function take_snapshot(){
+
+    save("myImage.png");
+}
